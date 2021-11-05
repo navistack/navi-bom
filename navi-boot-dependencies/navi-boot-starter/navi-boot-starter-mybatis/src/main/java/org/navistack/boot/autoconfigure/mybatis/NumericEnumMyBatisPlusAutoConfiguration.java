1bd3b682c3dd62a2f069e840f85538995f9e7787
ieu <5358482+ieu@users.noreply.github.com>
2021-11-05 12:04:11 +0800

    feat(starter-mybatis): NumericEnum handler

---
+++ navi-boot-dependencies/navi-boot-starter/navi-boot-starter-mybatis/src/main/java/org/navistack/boot/autoconfigure/mybatis/NumericEnumMyBatisPlusAutoConfiguration.java
@@ -0,0 +1,16 @@
+package org.navistack.boot.autoconfigure.mybatis;
+
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnBean(MybatisPlusAutoConfiguration.class)
+public class NumericEnumMyBatisPlusAutoConfiguration implements ConfigurationCustomizer {
+    @Override
+    public void customize(MybatisConfiguration configuration) {
+        configuration.setDefaultEnumTypeHandler(EnumDelegatingTypeHandler.class);
+    }
+}
