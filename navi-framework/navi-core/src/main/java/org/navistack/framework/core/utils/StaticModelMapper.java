0a134eb7031f8ebce44227244d4891e5f148ca49
ieu <5358482+ieu@users.noreply.github.com>
2021-11-05 11:37:35 +0800

    feat(core): simple ModelMapper wrapper

---
+++ navi-framework/navi-core/src/main/java/org/navistack/framework/core/utils/StaticModelMapper.java
@@ -0,0 +1,45 @@
+package org.navistack.framework.core.utils;
+
+import lombok.experimental.UtilityClass;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+
+import java.lang.reflect.Type;
+
+@UtilityClass
+public class StaticModelMapper {
+    private final ModelMapper instance;
+
+    static {
+        instance = new ModelMapper();
+        instance.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+    }
+
+    public ModelMapper instance() {
+        return instance;
+    }
+
+    public <D> D map(Object source, Class<D> destinationType) {
+        return instance.map(source, destinationType);
+    }
+
+    public <D> D map(Object source, Class<D> destinationType, String typeMapName) {
+        return instance.map(source, destinationType, typeMapName);
+    }
+
+    public void map(Object source, Object destination) {
+        instance.map(source, destination);
+    }
+
+    public void map(Object source, Object destination, String typeMapName) {
+        instance.map(source, destination, typeMapName);
+    }
+
+    public <D> D map(Object source, Type destinationType) {
+        return instance.map(source, destinationType);
+    }
+
+    public <D> D map(Object source, Type destinationType, String typeMapName) {
+        return instance.map(source, destinationType, typeMapName);
+    }
+}
