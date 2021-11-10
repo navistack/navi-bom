874c99e74c9e8cf9b0cee93f182a9ccd073da0c0
ieu <5358482+ieu@users.noreply.github.com>
2021-11-10 11:36:08 +0800

    fix(starter-web): hard-coded status on exception thrown

--- navi-boot-dependencies/navi-boot-starter/navi-boot-starter-web/src/main/java/org/navistack/boot/autoconfigure/web/rest/exceptionhandling/common/AdviceTrait.java
+++ navi-boot-dependencies/navi-boot-starter/navi-boot-starter-web/src/main/java/org/navistack/boot/autoconfigure/web/rest/exceptionhandling/common/AdviceTrait.java
@@ -8,6 +8,6 @@ public interface AdviceTrait {
     default <E> ResponseEntity<RestResult<Void, E>> toResponse(Throwable throwable, E error, HttpStatus httpStatus) {
         AdviceTraits.log(throwable, httpStatus);
 
-        return new ResponseEntity<> (RestResult.err(error), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<> (RestResult.err(error), httpStatus);
     }
 }
