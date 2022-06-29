d0c63e615cb200011c62b0fe690af88797dc3878
ieu <5358482+ieu@users.noreply.github.com>
2022-06-29 23:55:18 +0800

    fix: OperationLogAspect class not found exception

--- navi-boot-dependencies/navi-boot-autoconfigure/src/main/java/org/navistack/boot/autoconfigure/logging/LoggingAutoConfiguration.java
+++ navi-boot-dependencies/navi-boot-autoconfigure/src/main/java/org/navistack/boot/autoconfigure/logging/LoggingAutoConfiguration.java
@@ -1,15 +1,21 @@
 package org.navistack.boot.autoconfigure.logging;
 
+import org.navistack.framework.logging.OperationLog;
 import org.navistack.framework.logging.OperationLogAspect;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LoggingAutoConfiguration {
-    @Bean
-    @ConditionalOnMissingBean(OperationLogAspect.class)
-    public OperationLogAspect operationLogAspect() {
-        return new OperationLogAspect();
+    @Configuration
+    @ConditionalOnClass(OperationLog.class)
+    public static class OperationLogConfiguration {
+        @Bean
+        @ConditionalOnMissingBean(OperationLogAspect.class)
+        public OperationLogAspect operationLogAspect() {
+            return new OperationLogAspect();
+        }
     }
 }
