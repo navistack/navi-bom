57458c7710d740a622b0f6b35faf255fe46cd980
ieu <5358482+ieu@users.noreply.github.com>
2022-08-15 01:01:06 +0800

    feat: HashMapCacheService

---
+++ navi-framework/navi-core/src/main/java/org/navistack/framework/cache/HashMapCacheService.java
@@ -0,0 +1,89 @@
+package org.navistack.framework.cache;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class HashMapCacheService implements CacheService {
+    private final Map<String, Object> objectMap = new HashMap<>();
+    private final Map<String, Long> expirationMap = new HashMap<>();
+
+    @Getter
+    @Setter
+    private transient Clock clock = Clock.system(ZoneId.systemDefault());
+
+    @Override
+    public synchronized void set(String key, Object value) {
+        objectMap.put(key, value);
+    }
+
+    @Override
+    public synchronized void set(String key, Object value, Duration timeout) {
+        long expiration = Instant.now(clock).plus(timeout).toEpochMilli();
+        expirationMap.put(key, expiration);
+        objectMap.put(key, value);
+    }
+
+    @Override
+    public synchronized void set(String key, Object value, long timeout, TimeUnit unit) {
+        long expiration = Instant.now(clock).plus(unit.toMillis(timeout), ChronoUnit.MILLIS).toEpochMilli();
+        expirationMap.put(key, expiration);
+        objectMap.put(key, value);
+    }
+
+    @Override
+    public synchronized boolean setIfAbsent(String key, Object value) {
+        removeIfExpired(key, Instant.now(clock));
+        if (objectMap.containsKey(key)) {
+            return false;
+        }
+        set(key, value);
+        return true;
+    }
+
+    @Override
+    public synchronized boolean setIfAbsent(String key, Object value, long timeout, TimeUnit unit) {
+        removeIfExpired(key, Instant.now(clock));
+        if (objectMap.containsKey(key)) {
+            return false;
+        }
+        set(key, value, timeout, unit);
+        return true;
+    }
+
+    @Override
+    public synchronized <V> V get(String key, Class<V> clazz) {
+        removeIfExpired(key, Instant.now(clock));
+        return clazz.cast(objectMap.get(key));
+    }
+
+    @Override
+    public synchronized boolean delete(String key) {
+        expirationMap.remove(key);
+        objectMap.remove(key);
+        return true;
+    }
+
+    @Override
+    public synchronized <V> V getAndDelete(String key, Class<V> clazz) {
+        removeIfExpired(key, Instant.now(clock));
+        return clazz.cast(objectMap.remove(key));
+    }
+
+    private void removeIfExpired(String key, Instant now) {
+        long nowMilli = now.toEpochMilli();
+        Long expiration = expirationMap.get(key);
+        if (expiration != null && expiration < nowMilli) {
+            expirationMap.remove(key);
+            objectMap.remove(key);
+        }
+    }
+}
---
+++ navi-framework/navi-core/src/test/java/org/navistack/framework/cache/HashMapCacheServiceTest.java
@@ -0,0 +1,94 @@
+package org.navistack.framework.cache;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HashMapCacheServiceTest {
+    @Test
+    void set() {
+        HashMapCacheService service = new HashMapCacheService();
+        service.set("key", "value");
+        assertThat(service.get("key", String.class)).isEqualTo("value");
+    }
+
+    @Test
+    void setWithDuration() {
+        HashMapCacheService service = new HashMapCacheService();
+        service.setClock(Clock.fixed(Instant.parse("2011-12-03T10:15:30Z"), ZoneId.of("UTC")));
+        service.set("key", "value", Duration.ZERO);
+        service.setClock(Clock.fixed(Instant.parse("2011-12-03T10:15:31Z"), ZoneId.of("UTC")));
+        assertThat(service.get("key", String.class)).isNull();
+    }
+
+    @Test
+    void setWithTimeUnit() {
+        HashMapCacheService service = new HashMapCacheService();
+        service.setClock(Clock.fixed(Instant.parse("2011-12-03T10:15:30Z"), ZoneId.of("UTC")));
+        service.set("key", "value", 0, TimeUnit.SECONDS);
+        service.setClock(Clock.fixed(Instant.parse("2011-12-03T10:15:31Z"), ZoneId.of("UTC")));
+        assertThat(service.get("key", String.class)).isNull();
+    }
+
+    @Test
+    void setIfAbsent() {
+        HashMapCacheService service = new HashMapCacheService();
+        service.setIfAbsent("key", "value");
+        assertThat(service.setIfAbsent("key", "new value")).isFalse();
+        assertThat(service.setIfAbsent("new key", "new value")).isTrue();
+        assertThat(service.get("key", String.class)).isEqualTo("value");
+        assertThat(service.get("new key", String.class)).isEqualTo("new value");
+    }
+
+    @Test
+    void setIfAbsentWithDuration() {
+        HashMapCacheService service = new HashMapCacheService();
+        service.setClock(Clock.fixed(Instant.parse("2011-12-03T10:15:30Z"), ZoneId.of("UTC")));
+        assertThat(service.setIfAbsent("key", "value", Duration.ZERO)).isTrue();
+        service.setClock(Clock.fixed(Instant.parse("2011-12-03T10:15:31Z"), ZoneId.of("UTC")));
+        assertThat(service.setIfAbsent("key", "new value")).isTrue();
+        assertThat(service.get("key", String.class)).isEqualTo("new value");
+    }
+
+    @Test
+    void setIfAbsentWithTimeUnit() {
+        HashMapCacheService service = new HashMapCacheService();
+        service.setClock(Clock.fixed(Instant.parse("2011-12-03T10:15:30Z"), ZoneId.of("UTC")));
+        assertThat(service.setIfAbsent("key", "value", 0, TimeUnit.SECONDS)).isTrue();
+        service.setClock(Clock.fixed(Instant.parse("2011-12-03T10:15:31Z"), ZoneId.of("UTC")));
+        assertThat(service.setIfAbsent("key", "new value")).isTrue();
+        assertThat(service.get("key", String.class)).isEqualTo("new value");
+    }
+
+    @Test
+    void get() {
+        HashMapCacheService service = new HashMapCacheService();
+        service.set("key", "value");
+        assertThat(service.get("key", String.class)).isEqualTo("value");
+        Assertions.assertThatThrownBy(() -> service.get("key", Number.class)).isInstanceOf(ClassCastException.class);
+    }
+
+    @Test
+    void delete() {
+        HashMapCacheService service = new HashMapCacheService();
+        service.set("key", "value");
+        assertThat(service.get("key", String.class)).isEqualTo("value");
+        service.delete("key");
+        assertThat(service.get("key", String.class)).isNull();
+    }
+
+    @Test
+    void getAndDelete() {
+        HashMapCacheService service = new HashMapCacheService();
+        service.set("key", "value");
+        assertThat(service.getAndDelete("key", String.class)).isEqualTo("value");
+        assertThat(service.getAndDelete("key", String.class)).isNull();
+    }
+}
