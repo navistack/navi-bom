34a4a5dd781e3788152bd1b0170421d332cc5e75
ieu <5358482+ieu@users.noreply.github.com>
2026-03-21 23:30:13 +0800

    test(navi-boot-smoke-test-security): fix test failure

--- navi-boot-smoke-test/navi-boot-smoke-test-security/src/main/java/org/navistack/smoketest/security/SampleSecurityApplication.java
+++ navi-boot-smoke-test/navi-boot-smoke-test-security/src/main/java/org/navistack/smoketest/security/SampleSecurityApplication.java
@@ -3,8 +3,10 @@ package org.navistack.smoketest.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 @EnableMethodSecurity
 public class SampleSecurityApplication {
 
