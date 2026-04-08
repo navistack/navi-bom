b74b8269fa6c682eda5a674d47bfb3c5c81dfb9f
ieu <5358482+ieu@users.noreply.github.com>
2026-04-08 23:30:49 +0800

    fix(navi-ratelimit): fix fixed window rate limiter expiring time error

--- navi-framework/navi-ratelimit/src/main/resources/navi/scripts/fixedwindowratelimiter.lua
+++ navi-framework/navi-ratelimit/src/main/resources/navi/scripts/fixedwindowratelimiter.lua
@@ -1,13 +1,12 @@
---- Fixed Window Rate Limiter
+-- Fixed Window Rate Limiter
 -- @param KEYS[1] Key
 -- @param ARGV[1] Max requests per window
 -- @param ARGV[2] Expiration
 -- @return true if the request should be accepted or false otherwise
 
-if (tonumber(redis.call('GET', KEYS[1])) or 0) < tonumber(ARGV[1]) then
-    redis.call('INCR', KEYS[1])
-    redis.call('EXPIRE', KEYS[1], ARGV[2])
-    return true
-else
-    return false
+local current = redis.call('INCR', KEYS[1])
+if current == 1 then
+    redis.call('EXPIRE', KEYS[1], tonumber(ARGV[2]))
 end
+
+return current <= tonumber(ARGV[1])
