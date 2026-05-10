0105037c52438e57b6b1d57766056bd1570a65b7
ieu <5358482+ieu@users.noreply.github.com>
2026-05-10 23:30:13 +0800

    test(navi-boot-security): add `@SuppressWarnings("unchecked")` to enhance test clarity

--- navi-boot/navi-boot-security/src/test/java/org/navistack/boot/security/autoconfigure/SanitizationAutoConfigurationTest.java
+++ navi-boot/navi-boot-security/src/test/java/org/navistack/boot/security/autoconfigure/SanitizationAutoConfigurationTest.java
@@ -24,6 +24,7 @@ class SanitizationAutoConfigurationTest {
     }
 
     @Test
+    @SuppressWarnings("unchecked")
     void shouldApplyConfiguredPropertiesWhenSanitizationConfigured() {
         new WebApplicationContextRunner()
                 .withConfiguration(AutoConfigurations.of(SanitizationAutoConfiguration.class))
