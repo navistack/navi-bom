42113be7e97bd6e7d20e3327c33f6041ee8c19b0
ieu <5358482+ieu@users.noreply.github.com>
2026-05-30 23:30:19 +0800

    refactor(navi-boot-jackson): remove redundant `VERSION` definition in `RestResultModule`

--- navi-boot/navi-boot-jackson/src/main/java/org/navistack/framework/jackson/web/RestResultModule.java
+++ navi-boot/navi-boot-jackson/src/main/java/org/navistack/framework/jackson/web/RestResultModule.java
@@ -2,20 +2,11 @@ package org.navistack.framework.jackson.web;
 
 import org.navistack.framework.web.rest.RestErrResult;
 import org.navistack.framework.web.rest.RestOkResult;
-import tools.jackson.core.Version;
-import tools.jackson.core.util.VersionUtil;
 import tools.jackson.databind.module.SimpleModule;
 
 public class RestResultModule extends SimpleModule {
-    protected static final Version VERSION = VersionUtil.parseVersion(
-            "0.0.1-SNAPSHOT",
-            "org.navistack.framework",
-            "navi-web"
-    );
 
     public RestResultModule() {
-        super(VERSION);
-
         setMixInAnnotation(RestOkResult.class, RestOkResultMixIn.class);
         setMixInAnnotation(RestErrResult.class, RestErrResultMixIn.class);
     }
